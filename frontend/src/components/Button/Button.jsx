ec6c489d326f8f9b7eba2c9278be27d3bb3bdd49
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-06-29 20:35:03 +0100

    Update Button.jsx

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,1 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.css';
 
+const Button = ({
+  children,
+  onClick,
+  type = 'button'
+  variant = 'primary',
+  disabled = false,
+   ...rest
+ }) => {
+
+  const buttonClass = `btn btn-$ {variant}${disabled ?'btn-disabled': "} 
+ `;
+ return (
+ <button 
+ type={type}
+ className={buttonClass}
+ onClick={onClick}
+ disabled={disabled}
+  {...rest}
+  >
+{children} {
+  }
+</button>
+  );
+  };
+
+  Button.propTypes = {
+   children:
+   PropTypes.node.isRequired,
+    onClick: PropTypes.func,
+    type: PropTypes.oneOf{['button','submit', 'reset']),
+    variant:
+    PropTypes.oneOf(['primary', 'secondary', 'danger', 'outline']),
+    disabled: PropTypes.bool,
+    };
+    
+    export Default Button;
