ca9c1005e9f3e887ad2efa2a791ab5903a6002fa
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-06-29 20:49:45 +0100

    Update Button.styles.js

--- frontend/src/components/Button/Button.styles.js
+++ frontend/src/components/Button/Button.styles.js
@@ -1,1 +1,25 @@
+.btn {
+display:
+align-items:
+justify-content:
+gap:
+padding:
 
+font-family
+font-size
+font-weight
+line-height
+text-align
+text-decoration
+
+border
+border-radius
+background-color
+cursor
+
+transition:   ;
+}
+
+.btn:hover {
+text-decoration: ;
+}
