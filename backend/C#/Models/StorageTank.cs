9c3f538292aa6acd6673fb124ac97aa47882f6a8
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-04 20:23:49 +0100

    Update StorageTank.cs

--- backend/C#/Models/StorageTank.cs
+++ backend/C#/Models/StorageTank.cs
@@ -1,1 +1,11 @@
+namespace OilGasApi.Models;
 
+public class StorageTank
+{
+  public int id { get; set; }
+  public string TankName { get; set; } = string.Empty;
+  public double CurrentCapacity { get; set; }
+  public double MaxCapacity { get; set; }
+  public string Status { get; set; } =
+   "Stable"; 
+}
