0790a6428bd707640c7e4883f3b5aa2861240121
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-04 21:09:56 +0100

    Update index.php

--- frontend/includes/index.php
+++ frontend/includes/index.php
@@ -1,1 +1,26 @@
+<?php
+$apiUrl = 
+"http://localhost:5000/api/storage";
+$jsonData=
+file_get_contents{$apiUrl};
+$tanks = json_decode($jsonData, 
+true);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+<link rel="stylesheet" href="css/style.css">
+ </head>   
+<body>
+  <?php include 'includes/header.php';?>
+
+<main class="dashboard-grid">
+ <?php foreach($tanks as $tank): ?>
+<div class= "status-card" <?php
+
+  
+
+
+
 
