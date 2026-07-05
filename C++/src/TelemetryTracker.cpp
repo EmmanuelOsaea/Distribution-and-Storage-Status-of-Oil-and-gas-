9d12f375180dcd7704568c56bfe901446a91cc5d
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-05 18:18:31 +0100

    Update TelemetryTracker.cpp

--- C++/src/TelemetryTracker.cpp
+++ C++/src/TelemetryTracker.cpp
@@ -1,1 +1,19 @@
+#include ".../include/TelemtryTracker.hpp"
+Telemetry tracker::Telemetry tracker(std::string id, double current, double max)
+     :tankId(id), currentVolume(current), maxCapacity(max) {}
 
+double TelemetryTracker::calculateFillPercentage() const {
+  if (maxCapacity <= 0) return 0.0;
+return (currentVolume / maxCapacity * 100.0;
+}
+
+std::string TelemetryTracker::evaluateStatus() cons {
+     double percentage = calculateFillPercentage();
+
+
+
+void TelemetryTracker: updateVolume(double newVolume) {
+if (newVolume >= 0 && newVolume <= maxCapacity) {
+currentVolume = newVolume;
+}
+}
