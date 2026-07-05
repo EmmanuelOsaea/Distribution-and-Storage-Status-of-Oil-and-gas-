eaee014e1ca0138fb85b55b2375d28224247b8c5
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-05 16:48:14 +0100

    Set up telemetry processing in main function
    Initialize TelemetryTracker and print telemetry info.

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include "../include/Telemetry Tracker.hpp"
 
+int main() {
+TelemetryTracker tank01("TK-NORTH-01", 86000.0, 100000.0);
+
+std::cout << "--- Oil & Gas Telemetry Processing Subsystem ---"\n";
+std::cout << "Asset Monitored:" << tank01 get.TankId() << "\n";
+std::cout << "Utilization: " <<
+std::cout << "Operational Risk State: " <<
+
+return 0;
