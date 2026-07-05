ebc25595a85aaa22f525136b5971bf1942afe9af
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-05 14:24:31 +0100

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -1,1 +1,15 @@
 
+
+
+
+echo $tank['maxCapacity']; ?>
+Liters</p>
+       <span class ="badge"><?
+php echo
+htmlspecialchars($tank['status']);?
+></span>
+<div>
+<?php endforeach; ?>
+</main>
+</body>
+</html>
