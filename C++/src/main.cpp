9ce84be97476bfb035623fc3b7409d83de38d97e
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-05 17:46:13 +0100

    Fix output formatting in telemetry processing

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -4,9 +4,10 @@
 int main() {
 TelemetryTracker tank01("TK-NORTH-01", 86000.0, 100000.0);
 
-std::cout << "--- Oil & Gas Telemetry Processing Subsystem ---"\n";
+std::cout << "--- Oil & Gas Telemetry Processing Subsystem ---\n";
 std::cout << "Asset Monitored:" << tank01 get.TankId() << "\n";
-std::cout << "Utilization: " <<
-std::cout << "Operational Risk State: " <<
-
+std::cout << "Utilization: " << tank01 calculateFillPercentage() << "%\n">
+std::cout << "Operational Risk State: " << tank01 evaluateStatus()  << "%\n">
+  
 return 0;
+}
