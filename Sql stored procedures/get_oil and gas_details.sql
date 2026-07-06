6e25f133329ffe7f536f7da8e420d44d0a470be5
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-07 00:39:07 +0100

    Enhance stored procedure with error handling
    Updated the stored procedure to include error handling and modified date.

--- Sql stored procedures/get_oil and gas_details.sql
+++ Sql stored procedures/get_oil and gas_details.sql
@@ -8,4 +8,46 @@ AS
 
 
 Author: 
-Create: Date
+Create Date: 2026-07-06
+Description: Inserts or update s user record
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  IsActive = @IsActive
+  ModifiedDate =
+  GETUTCDATE()
+  WHERE UserId = @UserId;
+  END 
+  
+  BEGIN CATCH
+  IF @@TRANCOUNT > 0
+  BEGIN
+  ROLLBACK TRANSACTION;
+END
+
+DECLARE @ErrorMessage INT NVARCHAR(4000)=
+ERROR_MESSAGE();
+  
+DECLARE @ErrorSeverity INT =
+ERROR_SEVERITY();
+
+DECLARE @ErrorState INT =
+ERROR_STATE();
+  
+RAISERROR(@ErrorMessage,
+@ErrorSeverity, @ErrorState);
+ END CATCH
+END
+GO
