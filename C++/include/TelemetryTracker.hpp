c6781580d19786d088344a82589d09cf97505fac
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-06 20:46:18 +0100

    Add TelemetryTracker class definition

--- C++/include/TelemetryTracker.hpp
+++ C++/include/TelemetryTracker.hpp
@@ -1,1 +1,21 @@
+#ifndef TELEMETRY CHECKER HPP
+#define TELEMETRY TRACKER HPP
 
+#include <string>
+
+class TelemetryTracker {
+private:
+     std::string tankId;
+     double current volume;
+     double maxCapacity;
+
+public:
+     TelemetryTracker(std:: string id, double current, double max);
+double calculateFillPercentage() const;
+std:: string evaluateStatus const;
+
+std:: string getTankId() const;
+void updateVolume(double newVolume);
+};
+
+#endif
