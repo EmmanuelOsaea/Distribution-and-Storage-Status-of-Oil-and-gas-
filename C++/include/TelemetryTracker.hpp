b3c562dc12a99be79a93952e7a7503e41a76adb5
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-24 19:30:30 +0100

    Update TelemetryTracker.hpp

--- C++/include/TelemetryTracker.hpp
+++ C++/include/TelemetryTracker.hpp
@@ -1,20 +1,19 @@
-#ifndef TELEMETRY CHECKER HPP
-#define TELEMETRY TRACKER HPP
+#ifndef TELEMETRY_CHECKER_HPP
+#define TELEMETRY_TRACKER_HPP
 
 #include <string>
 
 class TelemetryTracker {
 private:
      std::string tankId;
-     double current volume;
+     double currentVolume;
      double maxCapacity;
 
 public:
-     TelemetryTracker(std:: string id, double current, double max);
-double calculateFillPercentage() const;
-std::string evaluateStatus()
-const;
+     TelemetryTracker(const std::string& id, double current, double max);
 
+double calculateFillPercentage() const;
+std::string evaluateStatus() const;
 
 std:: string getTankId() const;
 void updateVolume(double newVolume);
