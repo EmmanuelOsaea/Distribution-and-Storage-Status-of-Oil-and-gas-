6fba3e14ed6bb172d93b6a036ad8508ec94fcf41
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-24 19:57:33 +0100

    Fix method calls for TelemetryTracker

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "../include/Telemetry Tracker.hpp"
+#include "../include/TelemetryTracker.hpp"
 
 int main() {
 TelemetryTracker tank01("TK-NORTH-01", 86000.0, 100000.0);
 
 std::cout << "--- Oil & Gas Telemetry Processing Subsystem ---\n";
-std::cout << "Asset Monitored:" << tank01 get.TankId() << "\n";
-std::cout << "Utilization: " << tank01 calculateFillPercentage() << "%\n">
-std::cout << "Operational Risk State: " << tank01 evaluateStatus()  << "%\n">
+std::cout << "Asset Monitored: " << tank01.getTankId() << "\n";
+std::cout << "Utilization: " << tank01.calculateFillPercentage() << "%\n">
+std::cout << "Operational Risk State: " << tank01.evaluateStatus()  << "\n">
   
 return 0;
 }
