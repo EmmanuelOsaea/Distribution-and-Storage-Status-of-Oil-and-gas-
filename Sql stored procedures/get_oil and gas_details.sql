c4be9c395912ad05dea5fccd3bcdd326fda59756
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-24 23:32:46 +0100

    Refactor stored procedure for user upsert

--- Sql stored procedures/get_oil and gas_details.sql
+++ Sql stored procedures/get_oil and gas_details.sql
@@ -2,32 +2,21 @@ CREATE OR ALTER PROCEDURE
 dbo.usp_User_Upsert
 @UserId INT = NULL,
 @Email NVARCHAR(258)
-@isActive BIT = 1
+@IsActive BIT = 1
 AS
-/
-*******************************************************************************
-
-
-Author: Your Name
-Create Date: 2026-07-06
-Description: Inserts or update a user record.
-Prefix 'usp_' denotes a User Stored Procedure.
-‎*******************************************************************************/
-  
 BEGIN
-
-SET NOCOUNT ON;
-SET XACT ABORT ON;
+    SET NOCOUNT ON;
+    SET XACT_ABORT ON;
 
   
-  BEGIN TRY
-  BEGIN TRANSACTION
+    BEGIN TRY
+        BEGIN TRANSACTION;
   
  IF @UserId IS NULL
  BEGIN  
-  INSERT INTO dbo.Users(Email, isActive, CreatedDate)
+  INSERT INTO dbo.Users (Email, IsActive, CreatedDate)
      VALUES(@Email, @IsActive,
-GETUTCDATE()
+GETUTCDATE());
 END
 ELSE  
 BEGIN
@@ -36,14 +25,14 @@ BEGIN
   IsActive = @IsActive
   ModifiedDate =
   GETUTCDATE()
-  WHERE UserId = @UserId;
-  END 
-  
+WHERE UserId = @UserId;
+ END
+   
+COMMIT TRANSACTION;
+END TRY
   BEGIN CATCH
-  IF @@TRANCOUNT > 0
-  BEGIN
-  ROLLBACK TRANSACTION;
-END
+   IF @@TRANCOUNT > 0
+       ROLLBACK TRANSACTION;
 
 DECLARE @ErrorMessage INT NVARCHAR(4000)=
 ERROR_MESSAGE();
@@ -56,6 +45,6 @@ ERROR_STATE();
   
 RAISERROR(@ErrorMessage,
 @ErrorSeverity, @ErrorState);
- END CATCH
+    END CATCH
 END
 GO
